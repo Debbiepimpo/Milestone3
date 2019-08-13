764f917c134cd9ef9e73b00931e645f31ba23011
Ubuntu <ubuntu@ip-172-31-81-70.ec2.internal>
2019-08-13 14:46:55 +0000

    Added the login/register page

--- app.py
+++ app.py
@@ -4,21 +4,42 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY")
+# app.secret_key = os.getenv("SECRET_KEY")
 app.config["MONGO_DBNAME"]="CookBook"
-app.config["MONGO_URI"]=os.getenv("mongodb+srv://Debbiepimpo:Nora171215@my-first-cluster-gsys1.mongodb.net/CookBook?retryWrites=true&w=majority")
+app.config["MONGO_URI"]=os.getenv("MONGO_URI")
 
 mongo=PyMongo(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-   
-   
-@app.route("/login")
-def login():
-    return render_template("login.html")
     
+@app.route('/sign_in')
+def sign_in():
+    # Release the session variables
+    # session.pop('logged_in', None)
+    # session.pop('user', None)
+    return render_template('login.html')   
+   
+@app.route('/login',methods=['GET','POST'])
+def login():
+    users=mongo.db.users
+    username=request.form['user']
+    #Search for the inserted username in the DB
+    user=users.find_one({"user":username})
+    #Is user in the DB check if passwords match
+    if user:
+        password=user['password']
+        if request.form['password']==password:
+            session['user']=username
+            session['logged_in']=True
+            return redirect(url_for('home'))
+        else:
+            flash('Wrong password')    
+            return redirect(url_for('sign_in'))
+    else:        
+        flash('Wrong username')    
+        return redirect(url_for('sign_in'))  
 # New user registration template.
 @app.route('/new_user')
 def new_user():
@@ -30,7 +51,7 @@ def register():
     user={}
     users=mongo.db.users
     username=request.form['user']
-    # It checks if the user name is not being used before
+    # Checks if the user name is in use 
     if users.find_one({"user":username}):
         flash('Username not valid, already taken.')
         return redirect(url_for('new_user'))
@@ -40,7 +61,7 @@ def register():
         user["password"]=request.form['password']
         users.insert_one(user)
         flash('User created. You can now login.')
-        return redirect(url_for('autentication'))
+        return redirect(url_for('sign_in'))
             
 @app.route("/recipes")
 def recipes():
@@ -51,8 +72,7 @@ def favourites():
     return render_template("favourites.html")
     
     
-    
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
