4bd371dd73f98746ac1a9e394cf2d7b738ad06c0
Ubuntu <ubuntu@ip-172-31-81-70.ec2.internal>
2019-08-28 13:24:40 +0000

    correcting little details of CSS

--- app.py
+++ app.py
@@ -565,4 +565,4 @@ def removefavourite(recipe_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
